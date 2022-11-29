a29fb3c0883a4eb41a97abbc136d1c9d80f75e95
Janna Aquino <janna.aquinor@gmail.com>
2022-11-29 14:40:59 -0400

    invalid credentials

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,18 +2,18 @@ describe ('Login test', ()=>{
 //Hooks
 beforeEach(()=>{
     cy.visit('/')
+    const user1 = 'standard_user'
+    const user2 = 'problem_user'
+    const user3 = 'locked_out_user' //blocked user
+    const user4 = 'performance_glitch_user'
+    const pass ='secret_sauce'
 
 })
 
-    it('login succesfully',()=>{
+    it('TC1:login_succesfully',()=>{
         //arragement
-        
-        const user1 = 'standard_user'
-        const user2 = 'problem_user'
-        const user3 = 'locked_out_user' //blocked user
-        const user4 = 'performance_glitch_user'
+        const user1 = 'standard_user' 
         const pass ='secret_sauce'
-
         //action
         cy.get('[data-test="username"]').type(user1)
         cy.get('[data-test="password"]').type(pass)
@@ -21,10 +21,11 @@ beforeEach(()=>{
 
         //assert
         cy.get('.header_secondary_container').should('exist')
+       // cy.wait(3000);
 
     })
 
-    it('user_empty',()=>{
+    it('TC2:user_empty',()=>{
         //arragement
       
         const pass ='secret_sauce'
@@ -39,7 +40,7 @@ beforeEach(()=>{
 
     })
 
-    it('pass_empty',()=>{
+    it('TC3:pass_empty',()=>{
         //arragement
       
         //const pass ='secret_sauce'
@@ -55,6 +56,34 @@ beforeEach(()=>{
 
     })
 
+    it('TC4:credentials_empty',()=>{
+        //arragement
+      
+        //action
+      
+        cy.get('[data-test="login-button"]').click()
+
+        //assert
+        cy.get('.error-message-container').should('have.text', 'Epic sadface: Username is required')
+
+    })
+
+    it('TC5:invalid_credentials',()=>{
+        //arragement
+      
+        const user1 = 'Janna'
+        const pass ='test'
+
+        //action
+        cy.get('[data-test="username"]').type(user1)
+        cy.get('[data-test="password"]').type(pass)
+        cy.get('[data-test="login-button"]').click()
+
+        //assert
+        cy.get('.error-message-container').should('have.text','Epic sadface: Username and password do not match any user in this service')
+
+    })
+    
 
 })
 

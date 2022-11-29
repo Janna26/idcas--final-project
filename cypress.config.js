8b24e21015428ba1733665e24dc8e8c7b8baf61a
Janna Aquino <janna.aquinor@gmail.com>
2022-11-28 20:26:05 -0400

    basic login

--- cypress.config.js
+++ cypress.config.js
@@ -4,10 +4,11 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      
+     
     },
-    baseUrl: 'https://www.saucedemo.com/'
-  
+    baseUrl: 'https://www.saucedemo.com/',
+    chromeWebSecurity:false
+    
 },
-
+  
 });

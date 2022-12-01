083c4f10024576953ecff3b85dde1fb1b0fda3b6
Janna Aquino <janna.aquinor@gmail.com>
2022-12-01 16:12:55 -0400

    filter tests

--- cypress/e2e/filterTest.cy.js
+++ cypress/e2e/filterTest.cy.js
@@ -1,5 +1,6 @@
 
 //import { beforeEach } from 'mocha'
+
 import loginPage from './pages/loginPage.js'
 
 describe ('Filter test',()=>{
@@ -13,12 +14,28 @@ describe ('Filter test',()=>{
         loginPage.submit().click()
     
         cy.get('.header_secondary_container').should('exist')
-        //cy.wait(3000);
+        
     })
 
-    it ('home test header',()=>{
+    it ('home page',()=>{
         cy.get('.title').should('have.text', 'Products')
     })
+
+    it('Filter articles Name (A to Z)',()=>{ 
+       cy.get('.product_sort_container').select ('Name (A to Z)')
+      
+    })
     
+    it('Filter articles Name (Z to A)',()=>{ 
+        cy.get('.product_sort_container').select ('Name (Z to A)')
+     })
+
+    it('Filter articles Price (low to high)',()=>{ 
+        cy.get('.product_sort_container').select ('Price (low to high)')
+     })
+
+     it('Filter articles Price (high to low)',()=>{ 
+        cy.get('.product_sort_container').select ('Price (high to low)')
+     })
 })
 

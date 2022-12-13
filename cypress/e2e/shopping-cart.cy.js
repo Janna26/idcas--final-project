acd37da48c17ac2b93e72391b78dc4615bd4a162
Janna Aquino <janna.aquinor@gmail.com>
2022-12-13 19:57:27 -0400

    adding automation of TC5

--- cypress/e2e/shopping-cart.cy.js
+++ cypress/e2e/shopping-cart.cy.js
@@ -103,7 +103,7 @@ describe ('Purchase flow',()=>{
 
     })
 
-    it.only('TC4:Select more than one item and remove one or more from the cart.',
+    it('TC4:Select more than one item and remove one or more from the cart.',
     ()=>{
 
         cy.fixture('items').then((item)=>{
@@ -126,27 +126,42 @@ describe ('Purchase flow',()=>{
 
     })
 
-    it.skip('TC5: Select more than one item, remove from Description page and return to Products page.',()=>{
+    it.only('TC5: Select more than one item, remove from Description page and return to Products page.',
+    ()=>{
+        cy.fixture('items').then((item)=>{
+
+            for (let key = 0; key <4; key++) {
+
+              cy.get(item[Object.keys(item)[key]]).click();
+             }    
+        })
 
         cy.fixture('cart').then((cart)=>{
-           
-            
-            
+            cy.get(cart.cartLink).click();
+            cy.fixture('removeItems').then((remove)=>{
+
+                for (let key = 0; key <2; key++) {
+    
+                    cy.get(remove[Object.keys(remove)[key]]).click();
+                   }         
+            })
+
+            cy.get(cart.contShopping).click();
+            cy.get('.title').should('have.text', 'Products')
+
         })
+
+  
+
 
     })
 
     it.skip('TC6: Select more than one item and return to the cart, select n items and complete the flow.',()=>{
 
-        cy.fixture('cart').then((cart)=>{
-           
-            
-            
-        })
 
     })
 
-    it.skip('TC7: Carry out the purchase flow and do not enter the name in Checkout.',()=>{
+    it('TC7: Carry out the purchase flow and do not enter the name in Checkout.',()=>{
 
         cy.fixture('cart').then((cart)=>{
             
@@ -168,7 +183,7 @@ describe ('Purchase flow',()=>{
 
     })
 
-    it.skip('TC8:Carry out the purchase flow and do not enter the last name in Checkout.',()=>{
+    it('TC8:Carry out the purchase flow and do not enter the last name in Checkout.',()=>{
 
         cy.fixture('cart').then((cart)=>{
             cy.get('.inventory_item_label').contains('Sauce Labs Backpack')
@@ -190,7 +205,7 @@ describe ('Purchase flow',()=>{
 
     })
 
-    it.skip('TC9: Carry out the purchase flow and do not enter the zip code at Checkout.',()=>{
+    it('TC9: Carry out the purchase flow and do not enter the zip code at Checkout.',()=>{
 
         cy.fixture('cart').then((cart)=>{
             
